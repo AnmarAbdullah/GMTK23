78ed956824c1b980a0eb991f33b1978b2567db0c
Caimanned <superprunejuice@gmail.com>
2023-07-09 00:02:40 +0400

    Character movement with basic camera follow and whiteboxing colliders

---
+++ Assets/Kyle/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform playerPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+
+        transform.position = new Vector3(playerPosition.position.x,
+            playerPosition.position.y,
+            transform.position.z);
+    }
+}
